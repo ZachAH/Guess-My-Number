f3f9f20033fb75dc41f09190658fc9a3627e1e29
ZMAN <etta1055@gmail.com>
2021-05-21 10:53:22 -0500

    DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+//grabbing element with class message if id use #
+console.log(document.querySelector('.message').
+//reading the content of the text
+textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
