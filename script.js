a21cc94e09f40278cfd3a6e383e82c020109317d
ZMAN <etta1055@gmail.com>
2021-05-22 11:09:55 -0500

    added click function to man. the dom

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 'use strict';
-//grabbing element with class message if id use #
-console.log(document.querySelector('.message').
-//reading the content of the text
-textContent);
+// //grabbing element with class message if id use #
+// console.log(document.querySelector('.message').
+// //reading the content of the text
+// textContent);
 
-document.querySelector('.message').textContent = 'Correct Number!'
+// document.querySelector('.message').textContent = 'Correct Number!'
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+
+//adding onClick event to put the value in guess into the console
+//before that changing element to number
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    //if guess has no number it will change the text to no number because zero is a falsy value
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number😥!'
+    }
+});
+
