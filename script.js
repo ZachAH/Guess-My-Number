85cb06c8435f6a19842e1929b3f9fc8c1da5d1a1
ZMAN <etta1055@gmail.com>
2021-05-26 14:51:48 -0500

    added js for endgame

--- script.js
+++ script.js
@@ -11,6 +11,10 @@
 
 // document.querySelector('.guess').value = 23;
 
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 //adding onClick event to put the value in guess into the console
 //before that changing element to number
 document.querySelector('.check').addEventListener('click', function(){
@@ -19,7 +23,28 @@ document.querySelector('.check').addEventListener('click', function(){
 
     //if guess has no number it will change the text to no number because zero is a falsy value
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number😥!'
+        document.querySelector('.message').textContent = 'No number😥!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number! 🙌';
+
+    } else if (guess > secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too High 😠';
+            score --;
+            document.querySelector('.score').textContent =  score;
+        } else {
+              document.querySelector('.message').textContent = '🤯 You lost the game!';
+        }
+
+    } else if (guess < secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too Low 😠';
+            score --;
+            document.querySelector('.score').textContent =  score;
+         } else {
+            document.querySelector('.message').textContent = '🤯 You lost the game!';
+         }
+        
     }
 });
 
