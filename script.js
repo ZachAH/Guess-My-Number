0a9a3b9c3df32df72736a2016af68a7e5b48933b
ZMAN <etta1055@gmail.com>
2021-05-27 12:36:33 -0500

    added highscore function

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
 
 //adding onClick event to put the value in guess into the console
@@ -32,6 +33,11 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
     // if players guess is to high
     } else if (guess > secretNumber) {
